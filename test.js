9b9c575c3ad9016dd4c1124fb2788ecd276e521e
Ryan Zimmerman <opensrc@ryanzim.com>
2022-11-01 12:07:29 -0400

    tests: add test suite & CI (#10)
    * tests: add test suite
    
    * chore: add GitHub Actions CI; remove husky hooks
    
    * fix: use setImmediate in tests

---
+++ test.js
@@ -0,0 +1,51 @@
+/* eslint-env jest */
+const Redux = require('redux')
+const awaitState = require('.')
+
+const initialState = { enabled: false }
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'TOGGLE_ENABLED':
+      return { ...state, enabled: !state.enabled }
+    case 'OTHER_ACTION':
+      return { ...state, someOtherProperty: 42 }
+    default:
+      return state
+  }
+}
+
+const isEnabledSelector = (store) => !!store.enabled
+const isDisabledSelector = (store) => !store.enabled
+
+let store
+
+beforeEach(() => {
+  store = Redux.createStore(reducer)
+})
+
+test('awaitState waits for state to update', async () => {
+  // queue up actions
+  setImmediate(() => store.dispatch({ type: 'TOGGLE_ENABLED' }))
+
+  expect(store.getState().enabled).toBe(false)
+  await awaitState(store, isEnabledSelector)
+  expect(store.getState().enabled).toBe(true)
+})
+
+test('awaitState resolves immediately if the condition already exists', async () => {
+  await awaitState(store, isDisabledSelector) // shouldn't hang
+  expect(store.getState().enabled).toBe(false)
+})
+
+test('awaitState ignores irrelevant updates', async () => {
+  // queue up actions
+  setImmediate(() => {
+    store.dispatch({ type: 'OTHER_ACTION' })
+    setImmediate(() => store.dispatch({ type: 'TOGGLE_ENABLED' }))
+  })
+
+  expect(store.getState().enabled).toBe(false)
+  await awaitState(store, isEnabledSelector)
+  expect(store.getState().enabled).toBe(true)
+})
